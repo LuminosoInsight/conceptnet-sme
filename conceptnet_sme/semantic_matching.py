25e7ac598de7e83aa2cacbd25aaba1cf68d1a614
Robert E. Beaudoin <rbeaudoin@luminoso.com>
2018-04-09 18:08:19 -0400

    Use a DataLoader (and Dataset and Sampler) to parallelize generation of training batches.

--- conceptnet_sme/semantic_matching.py
+++ conceptnet_sme/semantic_matching.py
@@ -1,13 +1,21 @@
 import torch
+try:
+    torch.multiprocessing.set_start_method('spawn')
+except RuntimeError:
+    pass
 import torch.autograd as autograd
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import Sampler
 from conceptnet5.vectors.formats import save_hdf, save_npy
 import numpy as np
 import pandas as pd
 import random
 import pathlib
 import os
+import contextlib
+import time
 
 from conceptnet_sme.relations import (
     COMMON_RELATIONS, ALL_RELATIONS, SYMMETRIC_RELATIONS, ENTAILED_RELATIONS,
@@ -21,6 +29,7 @@ from conceptnet5.vectors.transforms import l2_normalize_rows
 RELATION_INDEX = pd.Index(COMMON_RELATIONS)
 N_RELS = len(RELATION_INDEX)
 INITIAL_VECS_FILENAME = get_data_filename('vectors/numberbatch-biased.h5')
+EDGES_FILENAME = get_data_filename('collated/sorted/edges-shuf.csv')
 MODEL_FILENAME = get_data_filename('sme/sme.model')
 NEG_SAMPLES = 5
 LANGUAGES_TO_USE = [
@@ -68,30 +77,211 @@ def _make_rel_chart():
 ENTAILED_INDICES, UNRELATED_INDICES = _make_rel_chart()
 
 
-def iter_edges_forever(filename):
-    """
-    Turn an iterator of ConceptNet edges into an iterator that runs forever
-    in a cycle.
-    """
-    while True:
-        yield from iter_edges_once(filename)
+@contextlib.contextmanager
+def stopwatch(consumer):
+    '''
+    After executing the managed block of code, call the provided consumer with 
+    two arguments, the start and end times of the execution of the block.
+    '''
+    start_time = time.perf_counter()
+    yield
+    end_time = time.perf_counter()
+    if consumer is not None:
+        consumer(start_time, end_time)
+    return
+
+class TimeAccumulator:
+    '''
+    A simple consumer for use with stopwatches, that accumulates the total 
+    elapsed time over multiple calls, and has a convenience method for printing 
+    the total time (and optionally resetting it).
+    '''
+    def __init__(self, initial_time=0.0):
+        self.accumulated_time = initial_time
+        return
+    def __call__(self, start_time, end_time):
+        self.accumulated_time += end_time - start_time
+        return
+    def print(self, caption, accumulated_time=None):
+        print('{} {} sec.'.format(caption, self.accumulated_time))
+        if accumulated_time is not None:
+            self.accumulated_time = accumulated_time
+        return
 
 
-def iter_edges_once(filename):
-    """
-    Iterate through the edges in ConceptNet, given an "edges-shuf.csv" file
-    that contains tab-separated edge data in random order.
-    """
-    for line in open(filename, encoding='utf-8'):
-        _assertion, relation, concept1, concept2, _rest = line.split('\t', 4)
-        yield (relation, concept1, concept2, 1.)
+class EdgeDataset(Dataset):
+    '''
+    Wrapper class (around an iteration over ConceptNet edges) that enables us 
+    to use a torch DataLoader to parallelize generation of training batches.
+    '''
+    def __init__(self, filename, index, n_terms, batch_size):
+        '''
+        Construct an edge dataset from a filename (of a tab-separated 
+        ConceptNet edge file, which should be in random order), a (pandas) 
+        index mapping (row) numbers to terms of the ConceptNet vocabulary, 
+        the (total) number of such terms, and a batch size. 
+        '''
+        super().__init__()
+        self.filename = filename
+        self.index = index
+        self.n_terms = n_terms
+        self.batch_size = batch_size
+        # Cache the edges in CPU memory as torch LongTensors,
+        # skipping edges we don't intend to process.
+        rel_indices = []
+        left_indices = []
+        right_indices = []
+        weights = []
+        for count, (rel, left, right, weight) in \
+          enumerate(self.iter_edges_once()):
+            if count % 1000000 == 0:
+                print('Read {} edges.'.format(count))
+            if rel not in COMMON_RELATIONS:
+                continue
+            if not ENTAILED_INDICES[rel]:
+                continue
+            rel_idx = RELATION_INDEX.get_loc(rel)
+            left = uri_prefix(left)
+            right = uri_prefix(right)
+            try:
+                left_idx = self.index.get_loc(left)
+                right_idx = self.index.get_loc(right)
+            except KeyError:
+                continue
+            rel_indices.append(rel_idx)
+            left_indices.append(left_idx)
+            right_indices.append(right_idx)
+            weights.append(weight)
+        if len(rel_indices) < 1:
+            print('No edges survived filtering; fitting is impossible!')
+            raise ValueError
+        self.rel_indices = torch.LongTensor(rel_indices)
+        self.left_indices = torch.LongTensor(left_indices)
+        self.right_indices = torch.LongTensor(right_indices)
+        self.edge_weights = torch.FloatTensor(weights)
+        return
+
+    def __len__(self):
+        return len(self.rel_indices)
+    
+    def iter_edges_once(self):
+        '''Return the edges from the data file.'''
+        with open(self.filename, encoding='utf-8') as fp:
+            for line in fp:
+                _assertion, relation, concept1, concept2, _rest = \
+                    line.split('\t', 4)
+                yield (relation, concept1, concept2, 1.)
+        return
+
+    def __getitem__(self, i_edge):
+        '''
+        Produce a positive example and weight and a batch of (of size 
+        NEG_SAMPLES) of negative examples, derived from the edge at the 
+        given index.  The return values are three torch variables containing 
+        the positive example, the negative examples, and the weights.
+        '''
+        rel_idx = self.rel_indices[i_edge]
+        left_idx = self.left_indices[i_edge]
+        right_idx = self.right_indices[i_edge]
+        weight = self.edge_weights[i_edge]
+        
+        rel = RELATION_INDEX[rel_idx]
+        
+        # Possibly swap the sides of a relation
+        if coin_flip() and rel in SYMMETRIC_RELATIONS:
+            left_idx, right_idx = right_idx, left_idx
+        
+        # Possibly replace a relation with a more general relation
+        if coin_flip():
+            rel_idx = random.choice(ENTAILED_INDICES[rel])
+            rel = COMMON_RELATIONS[rel_idx]
+        
+        pos_rels = [rel_idx]
+        pos_left = [left_idx]
+        pos_right= [right_idx]
+        weights = [weight]
+        
+        neg_rels = []
+        neg_left = []
+        neg_right = []
+
+        for iter in range(NEG_SAMPLES):
+            corrupt_rel_idx = rel_idx
+            corrupt_left_idx = left_idx
+            corrupt_right_idx = right_idx
+            
+            corrupt_which = random.randrange(5)
+            if corrupt_which == 0:
+                if rel not in SYMMETRIC_RELATIONS and coin_flip():
+                    corrupt_left_idx = right_idx
+                    corrupt_right_idx = left_idx
+                else:
+                    corrupt_rel_idx = random.choice(UNRELATED_INDICES[rel])
+            elif corrupt_which == 1 or corrupt_which == 2:
+                while corrupt_left_idx == left_idx:
+                    corrupt_left_idx = random.randrange(self.n_terms)
+            else:
+                while corrupt_right_idx == right_idx:
+                    corrupt_right_idx = random.randrange(self.n_terms)
+
+            neg_rels.append(corrupt_rel_idx)
+            neg_left.append(corrupt_left_idx)
+            neg_right.append(corrupt_right_idx)
+
+        pos_data = dict(rel=autograd.Variable(torch.LongTensor(pos_rels)),
+                        left=autograd.Variable(torch.LongTensor(pos_left)),
+                        right=autograd.Variable(torch.LongTensor(pos_right)))
+        neg_data = dict(rel=autograd.Variable(torch.LongTensor(neg_rels)),
+                        left=autograd.Variable(torch.LongTensor(neg_left)),
+                        right=autograd.Variable(torch.LongTensor(neg_right)))
+        weights = autograd.Variable(torch.FloatTensor(weights))
+        return dict(positive_data=pos_data,
+                    negative_data=neg_data,
+                    weights=weights)
+
+    def collate_batch(self, batch):
+        '''
+        Collates batches (as returned by a DataLoader that batches 
+        the outputs of calls to __getitem__) into tensors (as required 
+        by the train method of SemanticMatchingModel).
+        '''
+        pos_rels = torch.cat(list(x['positive_data']['rel'] for x in batch))
+        pos_left = torch.cat(list(x['positive_data']['left'] for x in batch))
+        pos_right = torch.cat(list(x['positive_data']['right'] for x in batch))
+        neg_rels = torch.cat(list(x['negative_data']['rel'] for x in batch))
+        neg_left = torch.cat(list(x['negative_data']['left'] for x in batch))
+        neg_right = torch.cat(list(x['negative_data']['right'] for x in batch))
+        weights = torch.cat(list(x['weights'] for x in batch))
+        pos_data = (pos_rels, pos_left, pos_right)
+        neg_data = (neg_rels, neg_left, neg_right)
+        return pos_data, neg_data, weights
+
+
+class CyclingSampler(Sampler):
+    '''
+    Like a sequential sampler, but these samplers cycle repeatedly over the 
+    data source, and so have infinite length.
+    '''
+    def __init__(self, data_source):
+        self.data_source = data_source
+        self.index = 0
+        return
+    def _next_index(self):
+        result = self.index
+        self.index = (self.index + 1) % len(self.data_source)
+        return result
+    def __iter__(self):
+        return iter(self._next_index, None) # never raises StopIteration!
+    def __len__(self):
+        return -1 # what else can we do?
+
 
 
 class SemanticMatchingModel(nn.Module):
     """
     The PyTorch model for semantic matching energy over ConceptNet.
     """
-    def __init__(self, frame, use_cuda=True, relation_dim=10, batch_size=100):
+    def __init__(self, frame, use_cuda=True, relation_dim=10, batch_size=4096):
         """
         Parameters:
 
@@ -136,7 +326,7 @@ class SemanticMatchingModel(nn.Module):
         self.rel_vecs = nn.Embedding(N_RELS, self.relation_dim)
 
         # Using CUDA to run on the GPU requires different data types
-        if use_cuda:
+        if use_cuda and torch.cuda.is_available():
             self.float_type = torch.cuda.FloatTensor
             self.int_type = torch.cuda.LongTensor
             self.term_vecs = self.term_vecs.cuda()
@@ -145,6 +335,18 @@ class SemanticMatchingModel(nn.Module):
         else:
             self.float_type = torch.FloatTensor
             self.int_type = torch.LongTensor
+
+        print('Initializing edge dataset ....')
+        dataset_accumulator = TimeAccumulator()
+        with stopwatch(dataset_accumulator):
+            self.dataset = EdgeDataset(
+                EDGES_FILENAME,
+                self.index,
+                self.n_terms, 
+                self.batch_size
+            )
+        dataset_accumulator.print('Edge dataset initialization took')
+        print('Edge dataset contains {} edges.'.format(len(self.dataset)))
 
         self.identity_slice = self.float_type(np.eye(self.term_dim))
         self.reset_synonym_relation()
@@ -156,12 +358,10 @@ class SemanticMatchingModel(nn.Module):
         self.truth_multiplier = nn.Parameter(self.float_type([5.]))
         self.truth_offset = nn.Parameter(self.float_type([-3.]))
 
-    def ltvar(self, numbers):
-        """
-        This is something we have to do a lot: take a list or a numpy array
-        of integers, and turn it into a Variable containing a LongTensor.
-        """
-        return autograd.Variable(self.int_type(numbers))
+        if use_cuda and torch.cuda.is_available():
+            self.cuda()
+            if torch.cuda.device_count() > 1:
+                self = nn.DataParallel(self)
 
     def reset_synonym_relation(self):
         """
@@ -210,97 +410,6 @@ class SemanticMatchingModel(nn.Module):
 
         return energy_b * self.truth_multiplier + self.truth_offset
 
-    def positive_negative_batch(self, edge_iterator):
-        """
-        Produce a batch of positive examples and a batch of negative examples,
-        expressed as PyTorch variables containing 1-D LongTensors (that is,
-        vectors of integers).
-
-        The negative batch will be NEG_SAMPLES times as large as the positive
-        batch.
-
-        Returns the positive batch, the negative batch, and the weights for
-        the batch (though currently these weights are always 1).
-        """
-        pos_rels = []
-        pos_left = []
-        pos_right = []
-
-        neg_rels = []
-        neg_left = []
-        neg_right = []
-
-        weights = []
-
-        for rel, left, right, weight in edge_iterator:
-            try:
-                if rel not in COMMON_RELATIONS:
-                    continue
-                if not ENTAILED_INDICES[rel]:
-                    continue
-
-                left = uri_prefix(left)
-                right = uri_prefix(right)
-
-                # Possibly swap the sides of a relation
-                if coin_flip() and rel in SYMMETRIC_RELATIONS:
-                    left, right = right, left
-
-                rel_idx = RELATION_INDEX.get_loc(rel)
-                left_idx = self.index.get_loc(left)
-                right_idx = self.index.get_loc(right)
-
-                # Possibly replace a relation with a more general relation
-                if coin_flip():
-                    rel_idx = random.choice(ENTAILED_INDICES[rel])
-                    rel = COMMON_RELATIONS[rel_idx]
-
-            except KeyError:
-                continue
-
-            pos_rels.append(rel_idx)
-            pos_left.append(left_idx)
-            pos_right.append(right_idx)
-            weights.append(weight)
-
-            for iter in range(NEG_SAMPLES):
-                corrupt_rel_idx = rel_idx
-                corrupt_left_idx = left_idx
-                corrupt_right_idx = right_idx
-
-                corrupt_which = random.randrange(5)
-                if corrupt_which == 0:
-                    if rel not in SYMMETRIC_RELATIONS and coin_flip():
-                        corrupt_left_idx = right_idx
-                        corrupt_right_idx = left_idx
-                    else:
-                        corrupt_rel_idx = random.choice(UNRELATED_INDICES[rel])
-                elif corrupt_which == 1 or corrupt_which == 2:
-                    while corrupt_left_idx == left_idx:
-                        corrupt_left_idx = random.randrange(self.n_terms)
-                else:
-                    while corrupt_right_idx == right_idx:
-                        corrupt_right_idx = random.randrange(self.n_terms)
-
-                neg_rels.append(corrupt_rel_idx)
-                neg_left.append(corrupt_left_idx)
-                neg_right.append(corrupt_right_idx)
-
-            if len(weights) == self.batch_size:
-                break
-
-        pos_data = (self.ltvar(pos_rels), self.ltvar(pos_left), self.ltvar(pos_right))
-        neg_data = (self.ltvar(neg_rels), self.ltvar(neg_left), self.ltvar(neg_right))
-        weights = autograd.Variable(self.float_type(weights))
-        return pos_data, neg_data, weights
-
-    def make_batches(self, edge_iterator):
-        """
-        An infinite iterator of training batches.
-        """
-        while True:
-            yield self.positive_negative_batch(edge_iterator)
-
     def show_debug(self, batch, energy, positive):
         """
         On certain iterations, we show the training examples and what the model
@@ -342,6 +451,13 @@ class SemanticMatchingModel(nn.Module):
         model.load_state_dict(torch.load(filename))
         return model
 
+    def ltvar(self, numbers):
+        """
+        This is something we have to do a lot: take a list or a numpy array
+        of integers, and turn it into a Variable containing a LongTensor.
+        """
+        return autograd.Variable(self.int_type(numbers))
+
     def evaluate_conceptnet(self, cutoff_value=-1, output_filename=None):
         """
         Use the SME model to "sanity-check" existing edges in ConceptNet.
@@ -352,7 +468,7 @@ class SemanticMatchingModel(nn.Module):
             out = open(output_filename, 'w', encoding='utf-8')
         else:
             out = None
-        for rel, left, right, weight in iter_edges_once(get_data_filename('collated/sorted/edges-shuf.csv')):
+        for rel, left, right, weight in self.dataset.iter_edges_once():
             try:
                 rel_idx = RELATION_INDEX.get_loc(rel)
                 left_idx = self.index.get_loc(left)
@@ -360,7 +476,9 @@ class SemanticMatchingModel(nn.Module):
             except KeyError:
                 continue
 
-            model_output = self(self.ltvar([rel_idx]), self.ltvar([left_idx]), self.ltvar([right_idx]))
+            model_output = self(self.ltvar([rel_idx]),
+                                self.ltvar([left_idx]),
+                                self.ltvar([right_idx]))
             value = model_output.data[0]
             assertion = assertion_uri(rel, left, right)
             if value < cutoff_value:
@@ -393,9 +511,19 @@ class SemanticMatchingModel(nn.Module):
         true_target = autograd.Variable(self.float_type([1] * self.batch_size))
         false_target = autograd.Variable(self.float_type([0] * self.batch_size))
         steps = 0
-        for pos_batch, neg_batch, weights in self.make_batches(
-            iter_edges_forever(get_data_filename('collated/sorted/edges-shuf.csv'))
-        ):
+
+        # Note that you want drop_last=False with a CyclingSampler.
+        data_loader = DataLoader(self.dataset, batch_size=self.batch_size,
+                                 drop_last=False, num_workers=10,
+                                 collate_fn=self.dataset.collate_batch,
+                                 sampler=CyclingSampler(self.dataset),
+                                 pin_memory=True)
+        for pos_batch, neg_batch, weights in data_loader:
+            if self.int_type == torch.cuda.LongTensor:
+                pos_batch = tuple(x.cuda(async=True) for x in pos_batch)
+                neg_batch = tuple(x.cuda(async=True) for x in neg_batch)
+            if self.float_type == torch.cuda.FloatTensor:
+                weights = weights.cuda(async=True)
             self.zero_grad()
             pos_energy = self(*pos_batch)
             neg_energy = self(*neg_batch)
@@ -416,6 +544,7 @@ class SemanticMatchingModel(nn.Module):
 
             losses.append(loss.data[0])
             steps += 1
+            
             if steps in (1, 10, 20, 50, 100) or steps % 100 == 0:
                 self.show_debug(neg_batch, neg_energy, False)
                 self.show_debug(pos_batch, pos_energy, True)
